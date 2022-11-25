145ffb675490f7eb8bd308bfca92a6ab0ed6550d
Matthew Wood <w.matthew.wood@gmail.com>
2022-11-24 20:20:34 -0500

    Toggle Modal Functionality working.

--- client/src/CalendarPage.js
+++ client/src/CalendarPage.js
@@ -1,15 +1,34 @@
+import styled from 'styled-components'
+import { useState } from 'react'
 import CalendarModuleHybrid from "./CalendarModuleHybrid";
 import AddEventModal from "./AddEventModal";
 
 const CalendarPage = () => {
 
+  const [ visible, setVisible ] = useState(false)
+
+  const toggleModal = () => { setVisible(!visible) }
+
   return(
     <>
       <h1>Welcome to my CalendarPage!</h1>
       <CalendarModuleHybrid></CalendarModuleHybrid>
-      <AddEventModal></AddEventModal>
+      <ModalHolder>
+      { 
+        ! visible 
+        ? null
+        : <AddEventModal/>
+      }
+      </ModalHolder>
+      <button onClick={toggleModal}>Beep.</button>
     </>
   )
 }
+
+const ModalHolder = styled.div`
+  position: absolute;
+  left: 800px;
+  top: 240px;
+`
 
 export default CalendarPage
