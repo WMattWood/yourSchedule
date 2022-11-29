ae2f4ae789f34ccf074c8aca9c5c4c7affb6fa53
Matthew Wood <w.matthew.wood@gmail.com>
2022-11-28 21:31:23 -0500

    Auth0 displays user data in console on the home page

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,12 +1,18 @@
 import LoginButton from "./login"
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Homepage = () => {
 
+  const { user, isAuthenticated, isLoading } = useAuth0()
 
   return(
     <>
       <h1>Welcome to my Homepage!</h1>
       <LoginButton/>
+      { ! isAuthenticated
+        ? null
+        : <>{console.log(user)}</>
+      }
     </>
     
   )
