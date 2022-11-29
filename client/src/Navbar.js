31951b9046cd26abebf044887fb20f596d16e87a
Matthew Wood <w.matthew.wood@gmail.com>
2022-11-29 15:54:08 -0500

    Made a change to the navBar so that it navigates to an existing event everytime you click on events.

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,20 +1,40 @@
 import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
-import RosterPage from './RosterPage';
 import LogoutButton from './logout';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useContext } from 'react';
+import { CalendarContext } from './CalendarContext';
 
 const Navbar = () => {
 
   const navigate = useNavigate();
   const { user, isAuthenticated, isLoading } = useAuth0()
+  const { monthlyCalendar } = useContext(CalendarContext)
 
   const linkToCalendar = () => {
     navigate("/calendar");
   };
 
-  const linkToEventDetails = () => {
-    navigate("/event/60b58fee-e351-4c95-8bb1-c8b2c1482aa7");
+  const linkToEventDetails = async () => {
+    let defaultEventId = ""
+
+    // if (monthlyCalendar) {
+    //   defaultEventId = monthlyCalendar[0]._id
+    // } else {
+    //   const availableEvents = await fetch(`/calendar/allevents`)
+    //   .then(res => res.json() )
+    //   .then(res => res.data)
+    
+    //   defaultEventId = availableEvents[0]._id 
+    // }
+    
+    const availableEvents = await fetch(`/calendar/allevents`)
+        .then(res => res.json() )
+        .then(res => res.data)
+      
+    defaultEventId = availableEvents[0]._id 
+
+    navigate(`/event/${defaultEventId}`);
   };
 
   const linkToRoster = () => {
