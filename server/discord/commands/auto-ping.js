070b9bd37cf29ce1427ede396afe4ffb39f691dd
Matthew Wood <w.matthew.wood@gmail.com>
2022-11-30 12:39:35 -0500

    Reducing down to essential discord logic.

--- server/discord/commands/auto-ping.js
+++ server/discord/commands/auto-ping.js
@@ -1,42 +1,42 @@
-// const { SlashCommandBuilder } = require('discord.js');
-
-// module.exports = {
-// 	data: new SlashCommandBuilder()
-// 		.setName('ping')
-// 		.setDescription('Replies with Pong!'),
-// 	async execute(interaction) {
-// 		await interaction.reply('Pong!');
-// 	},
-// };
-
 const https = require('https');
 
-var postData = JSON.stringify({
-  'content' : 'Hello Johnson!'
-});
 
-var options = {
-  hostname: "discord.com",
-  port: 443,
-  path: '/api/webhooks/1047304219876675684/AxTCEvuiwHJ4BfPkD47N7mjZewuDMX6LUhEbY3kR-7mDbyqWv5g_dVu6x60gjIMaqLMT',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-};
+const msgServerViaWebHook = (msg) => {
 
-var req = https.request(options, (res) => {
-console.log('statusCode:', res.statusCode);
-console.log('headers:', res.headers);
-
-res.on('data', (d) => {
-    process.stdout.write(d);
+  let postData = JSON.stringify({
+    'content' : `${msg}`
   });
-});
 
-req.on('error', (e) => {
-  console.error(e);
-});
+  let options = {
+    hostname: "discord.com",
+    port: 443,
+    path: '/api/webhooks/1047304219876675684/AxTCEvuiwHJ4BfPkD47N7mjZewuDMX6LUhEbY3kR-7mDbyqWv5g_dVu6x60gjIMaqLMT',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
 
-req.write(postData);
-req.end();
+  let req = https.request(options, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+    
+    res.on('data', (d) => {
+        process.stdout.write(d);
+      });
+    });
+    
+    req.on('error', (e) => {
+      console.error(e);
+    });
+    
+    req.write(postData);
+    req.end();
+
+}
+
+// msgServerViaWebHook("Howdy Fella")
+
+module.exports = {
+  msgServerViaWebHook
+}
