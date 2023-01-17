34f0f288831d5e8aa1de3efb1f709b0c7930a5ad
Matthew Wood <w.matthew.wood@gmail.com>
2023-01-17 09:56:43 -0500

    Commenting out Signup Page for now on main branch.

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,43 +1,43 @@
 import styled from "styled-components";
 import { useState } from "react"
 
-const Signup = () => {
+// const Signup = () => {
 
-  const [ formData, setFormData ] = useState({})
+//   const [ formData, setFormData ] = useState({})
 
-  const inputHandler = (ev, fieldName) => {
-    target = ev.currentTarget.value
-    setFormData({...formData, [fieldName]: target})
-  }
+//   const inputHandler = (ev, fieldName) => {
+//     target = ev.currentTarget.value
+//     setFormData({...formData, [fieldName]: target})
+//   }
 
-  return (
-    <Signupform>
-      <Question>
-        < InputForm type="" 
-                    name="name"
-                    id="name" 
-                    value={formData.name} 
-                    onChange={(ev) => inputHandler(ev, "name" )} required ></InputForm>
-        < InputForm type="" 
-                    name="name"
-                    id="name" 
-                    value={formData.name} 
-                    onChange={(ev) => inputHandler(ev, "name" )} required ></InputForm>
-        < InputForm type="" 
-                    name="name"
-                    id="name" 
-                    value={formData.name} 
-                    onChange={(ev) => inputHandler(ev, "name" )} required ></InputForm>
-        < InputForm type="" 
-                    name="name"
-                    id="name" 
-                    value={formData.name} 
-                    onChange={(ev) => inputHandler(ev, "name" )} required ></InputForm>
-      </Question>
-      <SubmitButton></SubmitButton>
-    </Signupform>
-  )
-}
+//   return (
+//     <Signupform>
+//       <Question>
+//         < InputForm type="" 
+//                     name="name"
+//                     id="name" 
+//                     value={formData.name} 
+//                     onChange={(ev) => inputHandler(ev, "name" )} required ></InputForm>
+//         < InputForm type="" 
+//                     name="name"
+//                     id="name" 
+//                     value={formData.name} 
+//                     onChange={(ev) => inputHandler(ev, "name" )} required ></InputForm>
+//         < InputForm type="" 
+//                     name="name"
+//                     id="name" 
+//                     value={formData.name} 
+//                     onChange={(ev) => inputHandler(ev, "name" )} required ></InputForm>
+//         < InputForm type="" 
+//                     name="name"
+//                     id="name" 
+//                     value={formData.name} 
+//                     onChange={(ev) => inputHandler(ev, "name" )} required ></InputForm>
+//       </Question>
+//       <SubmitButton></SubmitButton>
+//     </Signupform>
+//   )
+// }
 
 
 export default Signup;
