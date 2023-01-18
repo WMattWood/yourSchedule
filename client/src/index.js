2576f08e4781ab0487034c5c02b49af98db6ce39
Matt Wood <w.matthew.wood@gmail.com>
2023-01-18 15:17:26 -0500

    Moved Auth0 secrets to .env file

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,14 @@ import CalendarProvider from './CalendarContext';
 import { Auth0Provider } from "@auth0/auth0-react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const domain = process.env.REACT_APP_AUTH0_DOMAIN
+const clientId = process.env.REACT_APP_AUTH0_CLIENTID
 
 root.render(
-  
-  // <React.StrictMode>
   <>
-    {console.log("Index.js", `${process.env.REACT_APP_URL_BASE}/calendar`)}
     <Auth0Provider
-      domain="dev-45e85v0tlk073h45.us.auth0.com"
-      clientId="a7T9ZSkGnQdwzDJ0LpQgFRpeg9j06I3B"
+      domain={domain}
+      clientId={clientId}
       redirectUri={window.location.origin}
     >
       <CalendarProvider>
@@ -21,5 +20,4 @@ root.render(
       </CalendarProvider>
     </Auth0Provider>
   </>
-  // </React.StrictMode>
 );
