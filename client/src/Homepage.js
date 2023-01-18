3825c535bb937bdbed811080fbf112046d3538b0
Matt Wood <w.matthew.wood@gmail.com>
2023-01-18 13:19:11 -0500

    Adding a troubleshooting useEffect to the homepage.

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,9 +1,23 @@
 import LoginButton from "./login"
+import { useEffect } from "react"
 
 const Homepage = () => {
 
-  console.log("Greetings. Env is... ", `${process.env.REACT_APP_URL_BASE}`)
-  
+  console.log("Env is... ", `${process.env.REACT_APP_URL_BASE}`)
+  const endpoint = `${process.env.REACT_APP_URL_BASE}/members/allmembers`
+  console.log("Endpoint is...", endpoint)
+
+  useEffect( () => {
+    fetch(`${process.env.REACT_APP_URL_BASE}/members/allmembers`)
+      .then( res => {
+        console.log("All Members Response: ", res)
+        return res.json() 
+      })
+      .then( res => {
+        console.log("All Members Response pt 2?: ", res)
+      })
+  }, [] )
+
   // The homepage is literally just a login button!  
   // Users should only be routed here if they have not yet logged in.
   
