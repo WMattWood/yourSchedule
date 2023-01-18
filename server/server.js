9b4419192ebdab2d4edd8ea2f0804084150f3776
Matt Wood <w.matthew.wood@gmail.com>
2023-01-18 10:53:24 -0500

    Troubleshooting fetch requests on server.js

--- server/server.js
+++ server/server.js
@@ -17,6 +17,8 @@ express()
 
   // CORS management
   .all('*', (req, res, next) => {
+    console.log("Server request:", req)
+    console.log("Server response:", res)
     if (process.env.NODE_ENV === 'development')
       res.header({
         'Access-Control-Allow-Origin': 'http://localhost:3000',
