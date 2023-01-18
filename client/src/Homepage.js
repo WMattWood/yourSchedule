dcf3539d6d22504c92016fef3f9c9d3d72ce0698
Matt Wood <w.matthew.wood@gmail.com>
2023-01-18 13:24:15 -0500

    Commenting out the backend troubleshooting useEffect from homepage.

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -3,20 +3,20 @@ import { useEffect } from "react"
 
 const Homepage = () => {
 
-  console.log("Env is... ", `${process.env.REACT_APP_URL_BASE}`)
-  const endpoint = `${process.env.REACT_APP_URL_BASE}/members/allmembers`
-  console.log("Endpoint is...", endpoint)
+  // console.log("Env is... ", `${process.env.REACT_APP_URL_BASE}`)
+  // const endpoint = `${process.env.REACT_APP_URL_BASE}/members/allmembers`
+  // console.log("Endpoint is...", endpoint)
 
-  useEffect( () => {
-    fetch(`${process.env.REACT_APP_URL_BASE}/members/allmembers`)
-      .then( res => {
-        console.log("All Members Response: ", res)
-        return res.json() 
-      })
-      .then( res => {
-        console.log("All Members Response pt 2?: ", res)
-      })
-  }, [] )
+  // useEffect( () => {
+  //   fetch(`${process.env.REACT_APP_URL_BASE}/members/allmembers`)
+  //     .then( res => {
+  //       console.log("All Members Response: ", res)
+  //       return res.json() 
+  //     })
+  //     .then( res => {
+  //       console.log("All Members Response pt 2?: ", res)
+  //     })
+  // }, [] )
 
   // The homepage is literally just a login button!  
   // Users should only be routed here if they have not yet logged in.
