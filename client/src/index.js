3404ff0920a5205dc6ffce5243b19269ebf3b6b2
Matt Wood <w.matthew.wood@gmail.com>
2023-01-18 20:45:55 -0500

    Testing redirectURI

--- client/src/index.js
+++ client/src/index.js
@@ -8,13 +8,19 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENTID
 
+const redirectURI = ''
+process.env.NODE_ENV === 'development' 
+  ? redirectURI = "https:/yourschedule.onrender.com" : null
+process.env.NODE_ENV === 'production' 
+  ? redirectURI = "http:/localhost:3000/calendar" : null
+
 root.render(
   <>
     <Auth0Provider
       domain={domain}
       clientId={clientId}
       redirectUri={window.location.origin}
-    > {console.log(clientId)}
+    > {console.log(redirectURI)}
       <CalendarProvider>
         <App />
       </CalendarProvider>
