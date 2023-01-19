6c79a4ccacfbbc61a7160941f75bb57bc14a3e64
Matt Wood <w.matthew.wood@gmail.com>
2023-01-18 21:08:15 -0500

    Testing redirectURI

--- client/src/index.js
+++ client/src/index.js
@@ -8,12 +8,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENTID
 
-// const redirectURI = ''
-// if (process.env.NODE_ENV === 'development') {
-//   redirectURI = "https:/yourschedule.onrender.com"
-// } else if (process.env.NODE_ENV === 'production') {
-//   redirectURI = "http:/localhost:3000/calendar"
-// }
+let redirectURI = ''
+if (process.env.NODE_ENV === 'development') {
+  redirectURI = "https:/yourschedule.onrender.com"
+} else if (process.env.NODE_ENV === 'production') {
+  redirectURI = "http:/localhost:3000/calendar"
+}
 
 root.render(
   <>
@@ -21,7 +21,7 @@ root.render(
       domain={domain}
       clientId={clientId}
       redirectUri={window.location.origin}
-    > 
+    > {console.log(redirectURI)}
       <CalendarProvider>
         <App />
       </CalendarProvider>
