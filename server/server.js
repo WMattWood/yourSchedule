6ce26dfd5ac3b2e17aea6f680f2695e676f3e6e4
Matt Wood <w.matthew.wood@gmail.com>
2023-01-19 22:22:43 -0500

    Commented CORS stuff back in.

--- server/server.js
+++ server/server.js
@@ -15,22 +15,22 @@ express()
   .use(helmet())
   .use(morgan('tiny'))
 
-  // // CORS management
-  // .all('*', (req, res, next) => {
-  //   // console.log("Server request:", req)
-  //   // console.log("Server response:", res)
-  //   if (process.env.NODE_ENV === 'development')
-  //     res.header({
-  //       'Access-Control-Allow-Origin': 'http://localhost:3000',
-  //       'Access-Control-Allow-Headers': 'Origin, X-RequestedWith, Content-Type, Accept',
-  //   })
-  //   if (process.env.NODE_ENV === 'production')
-  //     res.header({
-  //       'Access-Control-Allow-Origin': 'https://yourschedule.onrender.com',
-  //       'Access-Control-Allow-Headers': 'Origin, X-RequestedWith, Content-Type, Accept',
-  //   })
-  //   next()
-  // })
+  // CORS management
+  .all('*', (req, res, next) => {
+    // console.log("Server request:", req)
+    // console.log("Server response:", res)
+    if (process.env.NODE_ENV === 'development')
+      res.header({
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
+        'Access-Control-Allow-Headers': 'Origin, X-RequestedWith, Content-Type, Accept',
+    })
+    if (process.env.NODE_ENV === 'production')
+      res.header({
+        'Access-Control-Allow-Origin': 'https://yourschedule.onrender.com',
+        'Access-Control-Allow-Headers': 'Origin, X-RequestedWith, Content-Type, Accept',
+    })
+    next()
+  })
 
   // initial test endpoint
   .get('/howdy', (req, res) => {
