9758f7a35978c0652712b7cbd394bf4db20bea79
Matthew Wood <w.matthew.wood@gmail.com>
2023-02-10 19:48:33 -0500

    Added a welcome message.

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import LoginButton from "./login"
 import { useAuth0 } from "@auth0/auth0-react";
 
@@ -13,8 +14,29 @@ const Homepage = () => {
         ? <LoginButton/>
         : null
       }
+      <DemoBanner>
+        Welcome to the demo of yourSchedule!  This app is currently hosted on a
+        free tier, and might take 30 seconds for the back end server to spin up.  
+        This means that it might take a moment for the events to become visible.  
+        <p>Feel free to look around, and if you have any questions you can contact 
+        me through my Github or LinkedIn!</p> 
+      </DemoBanner>
     </>
   )
 }
+
+const DemoBanner = styled.div`
+  font-family: var(--font-mono);
+  font-size: 24;
+  padding: 20px;
+  border: 2px solid black;
+  box-shadow: 4px 4px black;
+  width: 400px;
+  height: 200px;
+  position: relative;
+  top: 60px;
+  left: 60px;
+  background:var(--modal);
+`
 
 export default Homepage
